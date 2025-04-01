300d35d0731bc0480cec2fb224495c8983e8b652
ksdkaveesha <ksdkaveesha@gmail.com>
2025-04-01 12:52:05 +0530

    monthly report function

--- models/MonthlyReport.js
+++ models/MonthlyReport.js
@@ -1,19 +1,19 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/db.js";
 
-const MonthlyReport = sequelize.define("MonthlyReport", {
+const MonthlyReportNotify = sequelize.define("MonthlyReportNotify", {
     id: {
         type: DataTypes.INTEGER,
         autoIncrement: true,
         primaryKey: true,
     },
-    disease_confirmed: {
+    disease_name: {
         type: DataTypes.STRING,
         allowNull: false,
     },
 }, {
-    tableName: "communicable_disease_report", // Ensure it matches your DB table
+    tableName: "communicable_disease_notification", // Ensure it matches your DB table
     timestamps: true, // If your table does not have updatedAt
 });
 
-export default MonthlyReport;
+export default MonthlyReportNotify;
--- routes/Routes.js
+++ routes/Routes.js
@@ -3,6 +3,7 @@ import { createComplain } from "../controllers/ComplainController.js";
 import { getReportCounts  } from "../controllers/DashboardController.js";
 import { getNews } from "../controllers/NewsController.js";
 import { notifyUser, fetchNotifications } from "../controllers/NotificationController.js";
+import { fetchReportCounts } from "../controllers/MonthlyReportController.js";
 
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router.get("/monthlyreport", getReportCounts ); // Get monthly report data
 router.get("/news", getNews ); // Get news data
 router.post("/notification_send", notifyUser); // Send notification
 router.get("/notification/:userId", fetchNotifications); // Fetch user notifications
+router.get("/reportCounts", fetchReportCounts);
 
 export default router;
