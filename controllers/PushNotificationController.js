2fc7b6f8889cb151bc66ecd06fcc3d9bc425b900
thanujadev26 <thanujapriyadarshane26@gmail.com>
2025-04-07 02:05:24 +0530

    Notification is ok

--- controllers/PushNotificationController.js
+++ controllers/PushNotificationController.js
@@ -2,9 +2,7 @@ import PushNotification from '../models/PushNotification.js';
 
 export async function pushNotifications(req, res) {
   try {
-    const notifications = await PushNotification.findAll({
-      order: [['createdAt', 'DESC']],
-    });
+    const notifications = await PushNotification.findAll();
     res.status(200).json({ success: true, notifications });
   } catch (error) {
     res.status(500).json({ error: error.message });
