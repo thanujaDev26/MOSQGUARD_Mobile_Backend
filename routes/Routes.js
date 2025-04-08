dceca3fde35a7c6c5f40482f10915eaaeb2c763c
ksdkaveesha <ksdkaveesha@gmail.com>
2025-04-08 15:17:50 +0530

    monthly report

--- routes/Routes.js
+++ routes/Routes.js
@@ -3,6 +3,7 @@ import { createComplain,fetchComplaints } from "../controllers/ComplainControlle
 import { getNews } from "../controllers/EventsController.js";
 import { notifyUser, fetchNotifications } from "../controllers/NotificationController.js";
 import { getMessageCounts } from "../controllers/DashboardController.js";
+import { getMonthlyReport } from "../controllers/MonthlyReportController.js";
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.post("/complain", createComplain);
 router.get("/complain", fetchComplaints);
 router.get("/news", getNews);
 router.get("/message-counts", getMessageCounts);
+router.get("/monthly_report", getMonthlyReport);
 
 export default router;
